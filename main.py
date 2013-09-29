be0f0a84cb4ea7f7c0a76cc188593c3ad9f37d54
Alex Stelea <alex.stelea@gmail.com>
2013-09-29 00:03:24 -0400

    Added dashboard

--- main.py
+++ main.py
@@ -89,16 +89,188 @@ class MainHandler(webapp2.RequestHandler):
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
 
   <script src="/js/lastfm.js"></script>
-  <script src="/js/lastfm.api.md5.js">
-  
+  <script src="/js/lastfm.api.md5.js"></script>
 
-  </script>
+  <script src="js/raphael.js"></script>
+  <script src="js/justgage.1.0.1.min.js"></script>
 
+  <script src="js/popup.js"></script>
+  <script>
+  Raphael.fn.drawGrid = function (x, y, w, h, wv, hv, color) {
+            color = "rgba(0,0,0,0.1)";
+            var path = ["M", Math.round(x) + .5, Math.round(y) + .5, "L", Math.round(x + w) + .5, Math.round(y) + .5, Math.round(x + w) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y + h) + .5, Math.round(x) + .5, Math.round(y) + .5],
+                rowHeight = h / hv,
+                columnWidth = w / wv;
+            for (var i = 1; i < hv; i++) {
+                path = path.concat(["M", Math.round(x) + .5, Math.round(y + i * rowHeight) + .5, "H", Math.round(x + w) + .5]);
+            }
+            for (i = 1; i < wv; i++) {
+                path = path.concat(["M", Math.round(x + i * columnWidth) + .5, Math.round(y) + .5, "V", Math.round(y + h) + .5]);
+            }
+            return this.path(path.join(",")).attr({stroke: color});
+        };
+
+        $(function () {
+            $("#data").css({
+                position: "absolute",
+                left: "-9999em",
+                top: "-9999em"
+            });
+        });
+
+        window.onload = function () {
+            function getAnchors(p1x, p1y, p2x, p2y, p3x, p3y) {
+                var l1 = (p2x - p1x) / 2,
+                    l2 = (p3x - p2x) / 2,
+                    a = Math.atan((p2x - p1x) / Math.abs(p2y - p1y)),
+                    b = Math.atan((p3x - p2x) / Math.abs(p2y - p3y));
+                a = p1y < p2y ? Math.PI - a : a;
+                b = p3y < p2y ? Math.PI - b : b;
+                var alpha = Math.PI / 2 - ((a + b) % (Math.PI * 2)) / 2,
+                    dx1 = l1 * Math.sin(alpha + a),
+                    dy1 = l1 * Math.cos(alpha + a),
+                    dx2 = l2 * Math.sin(alpha + b),
+                    dy2 = l2 * Math.cos(alpha + b);
+                return {
+                    x1: p2x - dx1,
+                    y1: p2y + dy1,
+                    x2: p2x + dx2,
+                    y2: p2y + dy2
+                };
+            }
+            // Grab the data
+            var labels = [],
+                data = [];
+            $("#data tfoot th").each(function () {
+                labels.push($(this).html());
+            });
+            $("#data tbody td").each(function () {
+                data.push($(this).html());
+            });
+            
+            // Draw
+            var width = $("#holder").width() - 80,
+                height = 250,
+                leftgutter = 0,
+                bottomgutter = 20,
+                topgutter = 20,
+                colorhue = .6 || Math.random(),
+                color = "hsl(" + [colorhue, .5, .5] + ")",
+                r = Raphael("holder", width, height),
+                txt = {font: '12px Helvetica, Arial', fill: "#000"},
+                txt1 = {font: '10px Helvetica, Arial', fill: "#000"},
+                txt2 = {font: '12px Helvetica, Arial', fill: "#000"},
+                txt3 = {font: '12px Helvetica, Arial', fill: "#fff"},
+                X = (width - leftgutter) / labels.length,
+                labels3 = r.set(),
+                max = Math.max.apply(Math, data),
+                Y = (height - bottomgutter - topgutter) / max;
+
+            
+            r.drawGrid(leftgutter + X * .5 + .5, topgutter + .5, width - leftgutter - X, height - topgutter - bottomgutter, 10, 10, "#000");
+            var path = r.path().attr({stroke: color, "stroke-width": 4, "stroke-linejoin": "round"}),
+                bgp = r.path().attr({stroke: "none", opacity: .3, fill: color}),
+                label = r.set(),
+
+                lx = 0, ly = 0,
+                is_label_visible = false,
+                leave_timer,
+                blanket = r.set();
+            label.push(r.text(60, 12, "24 hits").attr(txt3));
+            label.push(r.text(60, 27, "22 September 2008").attr(txt1).attr({fill: color}));
+            label.hide();
+            label2 = r.set()
+            label2.push(r.text(30, 45, "Teacher").attr(txt1).attr({fill: color}));
+            label2.push(r.text(30, 57, "Training").attr(txt1).attr({fill: color}));
+            label2.push(r.text(625, 8, "Educational").attr(txt1).attr({fill: color}));
+            label2.push(r.text(625, 20, "Materials").attr(txt1).attr({fill: color}));
+            label2.push(r.text(425, 33, "Increase Education").attr(txt1).attr({fill: color}));
+            label2.push(r.text(425, 45, "Expenses").attr(txt1).attr({fill: color}));
+
+            var frame = r.popup(100, 100, label, "right").attr({fill: "#000", stroke: "#666", "stroke-width": 2, "fill-opacity": .7}).hide();
+
+            var p, bgpp;
+            for (var i = 0, ii = labels.length; i < ii; i++) {
+                var y = Math.round(height - bottomgutter - Y * data[i]),
+                    x = Math.round(leftgutter + X * (i + .5)),
+                    t = r.text(x, height - 6, labels[i]).attr(txt).toBack();
+                if (!i) {
+                    p = ["M", x, y, "C", x, y];
+                    bgpp = ["M", leftgutter + X * .5, height - bottomgutter, "L", x, y, "C", x, y];
+                }
+                if (i && i < ii - 1) {
+                    var Y0 = Math.round(height - bottomgutter - Y * data[i - 1]),
+                        X0 = Math.round(leftgutter + X * (i - .5)),
+                        Y2 = Math.round(height - bottomgutter - Y * data[i + 1]),
+                        X2 = Math.round(leftgutter + X * (i + 1.5));
+                    var a = getAnchors(X0, Y0, x, y, X2, Y2);
+                    p = p.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+                    bgpp = bgpp.concat([a.x1, a.y1, x, y, a.x2, a.y2]);
+                }
+                var dot = r.circle(x, y, 4).attr({fill: "#333", stroke: color, "stroke-width": 2});
+                blanket.push(r.rect(leftgutter + X * i, 0, X, height - bottomgutter).attr({stroke: "none", fill: "#fff", opacity: 0}));
+                var rect = blanket[blanket.length - 1];
+                (function (x, y, data, lbl, dot) {
+                    var timer, i = 0;
+                    rect.hover(function () {
+                        clearTimeout(leave_timer);
+                        var side = "right";
+                        if (x + frame.getBBox().width > width) {
+                            side = "left";
+                        }
+                        var ppp = r.popup(x, y, label, side, 1),
+                            anim = Raphael.animation({
+                                path: ppp.path,
+                                transform: ["t", ppp.dx, ppp.dy]
+                            }, 200 * is_label_visible);
+                        lx = label[0].transform()[0][1] + ppp.dx;
+                        ly = label[0].transform()[0][2] + ppp.dy;
+                        frame.show().stop().animate(anim);
+                        label[0].attr({text: data + " %"}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
+                        label[1].attr({text: lbl}).show().stop().animateWith(frame, anim, {transform: ["t", lx, ly]}, 200 * is_label_visible);
+                        dot.attr("r", 6);
+                        is_label_visible = true;
+                    }, function () {
+                        dot.attr("r", 4);
+                        leave_timer = setTimeout(function () {
+                            frame.hide();
+                            label[0].hide();
+                            label[1].hide();
+                            is_label_visible = false;
+                        }, 1);
+                    });
+                })(x, y, data[i], labels[i], dot);
+            }
+            p = p.concat([x, y, x, y]);
+            bgpp = bgpp.concat([x, y, x, y, "L", x, height - bottomgutter, "z"]);
+            path.attr({path: p});
+            bgp.attr({path: bgpp});
+            frame.toFront();
+            label[0].toFront();
+            label[1].toFront();
+            blanket.toFront();
+        };
+
+</script>
+ <style type="text/css">
+
+rect.a {
+        fill: green;
+      }
+      rect.b {
+        fill: orange;
+      }
+      rect.c {
+        fill: red;
+      }
+
+
+    </style>
 </head>
 
 <body>
 
-<div class="navbar">
+<div class="navbar navbar-inverse">
   <div class="navbar-inner">
     <a class="brand" href="#">Title</a>
     <ul class="nav">
@@ -112,8 +284,77 @@ class MainHandler(webapp2.RequestHandler):
 
 
   <div class="table well offset0">
+    <div id="gauge" style="display: block; margin: 0 auto; width:400px; height:320px"></div>
+    <div style="width: 800px; margin-top: 50px;  margin: 0 auto; height: 200px;">
+      <div id="song-gauge" style="position: relative; float: left; width:200px; height:160px"></div>  
+      <div id="sleep-gauge" style="position: relative; float: left;width:200px; height:160px"></div> 
+      <div id="activity-gauge" style="position: relative; float: left; width:200px; height:160px"></div> 
+      <div id="calendar-gauge" style="position: relative; float: left; width:200px; height:160px"></div>
+    </div>
+     
+
+
 
   </div>
+
+  <table id="data">
+                <h4 style="margin-bottom: 0px; margin-top: 70px;" align="center">Primary School Completion Rate</h4>    
+                    <tfoot>
+                        <tr>
+                            <th>1990</th>
+                            <th>1991</th>
+                            <th>1992</th>
+                            <th>1993</th>
+                            <th>1994</th>
+                            <th>1995</th>
+                            <th>1996</th>
+                            <th>1997</th>
+                            <th>1998</th>
+                            <th>1999</th>
+                            <th>2000</th>
+                            <th>2001</th>
+                            <th>2002</th>
+                            <th>2003</th>
+                            <th>2004</th>
+                            <th>2005</th>
+                            <th>2006</th>
+                            <th>2007</th>
+                            <th>2008</th>
+                            <th>2009</th>
+                            <th>2010</th>
+                            <th>2011</th>              
+                            
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>75.06</td>
+                            <td>79.76</td>
+                            <td>81.71</td>
+                            <td>83.56</td>
+                            <td>82.02</td>
+                            <td>80.60</td>
+                            <td>81.57</td>
+                            <td>81.57</td>
+                            <td>81.57</td>
+                            <td>81.46</td>
+                            <td>86.92</td>
+                            <td>90.55</td>
+                            <td>93.33</td>
+                            <td>93.33</td>
+                            <td>94.87</td>
+                            <td>94.17</td>
+                            <td>90.47</td>
+                            <td>92.39</td>
+                            <td>93.67</td>
+                            <td>96.32</td>
+                            <td>96.28</td>
+                            <td>99.09</td>
+                        </tr>
+                    </tbody>
+                </table>   
+                <div id="holder"></div>
+
   <h2 class="span10">Hello World</h2>
 </div>
 </body>
@@ -121,6 +362,7 @@ class MainHandler(webapp2.RequestHandler):
 
 <script type="text/javascript">
 
+           
  var lastfm = new LastFM({
       apiKey    : '641fb60a2b735363bbc1159bf090156c',
       apiSecret : 'faa27263fe6eed36d8c89f808b4c5473',
@@ -161,6 +403,9 @@ sleep_score = function(data) {
   }
   return data['sleep']/data['sleep_cnt'];
 }
+
+
+
 
 fetch_team = function(tid, sel) {
 
@@ -239,6 +484,43 @@ $(document).ready(function() {
   fetch_team(6, '#c6');
 
 });
+
+var g = new JustGage({
+    id: "gauge", 
+    value: 67, 
+    min: 0,
+    max: 100,
+    title: "Stress Score"
+  });   
+
+var g = new JustGage({
+    id: "song-gauge", 
+    value: 40, 
+    min: 0,
+    max: 100,
+    title: "Song Score"
+  });  
+var g = new JustGage({
+    id: "sleep-gauge", 
+    value: 50, 
+    min: 0,
+    max: 100,
+    title: "Sleep Score"
+  });  
+var g = new JustGage({
+    id: "activity-gauge", 
+    value: 74, 
+    min: 0,
+    max: 100,
+    title: "Activity Score"
+  });  
+var g = new JustGage({
+    id: "calendar-gauge", 
+    value: 92, 
+    min: 0,
+    max: 100,
+    title: "Schedule Score"
+  });  
 
 </script>
 
